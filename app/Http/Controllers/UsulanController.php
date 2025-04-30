3d0b2a88f366df7c5f005e0adbd7c8d37d9657a7
mfahmifadhs <mfahmifadh2@gmail.com>
2025-04-30 13:55:01 +0700

    Update

--- app/Http/Controllers/UsulanController.php
+++ app/Http/Controllers/UsulanController.php
@@ -635,7 +635,7 @@ class UsulanController extends Controller
             UsulanBbm::where('usulan_id', $id)->delete();
 
             Usulan::where('id_usulan', $id)->update([
-                'tanggal_selesai'   => $request->bulan_permintaan,
+                'tanggal_selesai'   => Carbon::parse($request->bulan_permintaan . '-01') : null,
             ]);
 
             $aadb = $request->aadb;
